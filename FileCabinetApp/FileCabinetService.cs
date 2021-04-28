d6d5a4d575224a920962b5fa898be3ba369df8b9
“Sebik <waffilka@gmail.comSebik Georgegit config --global user.email waffilka@gmail.com>
2021-04-28 17:14:33 +0300

    Add FileCabinetService.cs file.

---
+++ FileCabinetApp/FileCabinetService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCabinetApp
+{
+    public class FileCabinetService
+    {
+        private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+
+        public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+
+        public FileCabinetRecord[] GetRecords()
+        {
+            // TODO: добавьте реализацию метода
+            return new FileCabinetRecord[] { };
+        }
+
+        public int GetStat()
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+    }
+}
